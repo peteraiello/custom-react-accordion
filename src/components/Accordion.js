4fb51b60c653e8f047bdea00e656746cd2cb0de6
Peter Aiello <peteraiello@Peters-Mac-mini.fritz.box>
2021-02-04 21:18:34 +0000

    add keyboard events

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,26 +4,43 @@ import data from '../data.json';
 const Accordion = () => {
     
     const [active, setActive] = useState(0);
+    const dataCount = data.length -1;
 
-    const eventHandler = (index) => {
-        console.log(index);
+    const eventHandler = (e, index) => {
+        e.preventDefault();
         setActive(index);
     }
 
+    const keyPressed = (e) => {
+        e.preventDefault();
+        console.log(dataCount);
+        let key = e.key;
+        let arrowUp = 'ArrowUp';
+        let arrowDown = 'ArrowDown';
+        if(key === arrowDown && active < dataCount) {
+            setActive(active +1);
+        }
+        if(key === arrowUp && active > 0) {
+            setActive(active -1);
+        }
+    }
+
     return(
-        <form onClick={(e) => e.preventDefault()}>
-        { data.map((tab, index) => (
-            <div key={index}>
-                <button onClick={() => eventHandler(index)}
-                className={index === active ? 'active' : 'inactive'}
-                >
-                {tab.title}
-                <span className={index === active ? 'minus' : 'plus'}></span>
-                </button>
-                { index === active ? <div className="panel"> {tab.description} </div> : ''}
-            </div>
-            ))}
-        </form>
+        <div onKeyUp={(e) => keyPressed(e)}>
+            <form>
+            { data.map((tab, index) => (
+                <div key={index}>
+                    <button onClick={(e) => eventHandler(e, index)}
+                    className={index === active ? 'active' : 'inactive'}
+                    >
+                    {tab.title}
+                    <span className={index === active ? 'plus' : 'minus'}></span>
+                    </button>
+                    { index === active ? <div className="panel"> {tab.description} </div> : ''}
+                </div>
+                ))}
+            </form>
+        </div>
     );
 }
 
